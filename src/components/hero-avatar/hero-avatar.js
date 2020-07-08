429eadb412f5b30d4e467aa6bdd5e96893949608
Joe Faulstick <jfaulstick@gmail.com>
2020-07-07 22:03:55 -0400

    Removed LINK reference. Added alt tag to hero-avatar img

--- src/components/hero-avatar/hero-avatar.js
+++ src/components/hero-avatar/hero-avatar.js
@@ -3,7 +3,7 @@ import React from "react";
 export const HeroAvatar = () => {
     return (
         <div className='hero-avatar--container'>
-            <img className='hero-avatar--image' src='./assets/img/portrait-joe.png' />
+            <img className='hero-avatar--image' alt='joe' src='./assets/img/portrait-joe.png' />
         </div>
     )
 }
