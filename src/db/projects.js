6b7c03bca26fa5dc0f9e4c555b7f170eb1a8b17c
Joe Faulstick <jfaulstick@gmail.com>
2020-07-09 12:22:00 -0400

    Fixed relative paths for gh deployment

--- src/db/projects.js
+++ src/db/projects.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
     title: "Quizit",
-    logo: "/assets/img/quizit-logo.png",
+    logo: "./assets/img/quizit-logo.png",
     displayBoth: false,
-    images: ["/assets/img/quizit1.png", "/assets/img/quizit3.png"],
+    images: ["./assets/img/quizit1.png", "./assets/img/quizit3.png"],
     paras: [
       `QuizIt! was my final project for the Berkeley Coding Camp class. QuizIt! is a social trivia game where players can join in and compete against one another in a social environment.`,
       `QuizIt! Is powered by Open Trivia Database’s library of community provided questions and includes a live chat room for in-game communication among the players. The game loop and state machine was designed using a server / client model with Socket.io used as our communication protocal.`,
@@ -14,9 +14,9 @@ export const projects = [
   },
   {
     title: "Code Cats",
-    logo: "/assets/img/codecats-logo.jpg",
+    logo: "./assets/img/codecats-logo.jpg",
     displayBoth: true,
-    images: ["/assets/img/codecats1.jpg"],
+    images: ["./assets/img/codecats1.jpg"],
     paras: [
       `Code Cats was my second team project for the Berkeley Coding Camp class. Our goal in creating Code Cats was to create a social space where our cohort could interact outside of class.`,
       `The web application includes a 'Facebook-like' timeline with comments and a profile customizer with authentication through Google oAuth.`,
@@ -28,7 +28,7 @@ export const projects = [
   {
     title: "Scrape Da News",
     displayBoth: false,
-    images: ['/assets/img/scrapedanews1.jpg'],
+    images: ['./assets/img/scrapedanews1.jpg'],
     paras: [
       `Scrape Da News was an assignment I received where I was instructed to create my own web scraper, using the Cheerio JavaScript library and MongoDB for non-relational storage. I chose Boston.com's News section as the target website. Visitors can scrape new articles, save favorites, and add notes to their saved articles.`,
     ],
